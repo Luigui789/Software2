203d40b9990d8876fa408771ff9d8e97fec2d974
Luis <luisfernandojose2004@gmail.com>
2025-06-22 21:44:36 -0600

    Soy imbecil2

--- Software2/settings.py
+++ Software2/settings.py
@@ -173,7 +173,7 @@ else:
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6377/1')], # Usa REDIS_URL para Railway
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379/1')], # Usa REDIS_URL para Railway
         },
     },
 }
